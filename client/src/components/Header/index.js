3caeead9977080c22367c1a7bbfac6660a91b6a1
Vivi Cowan <vivicare@gmail.com>
2021-08-25 14:51:07 -0500

    logo updated

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import yapilogo from "../../assets/images/yapi.png";
+import yapilogo from "../../assets/images/yapi-logo.png";
 import Auth from "../../utils/auth";
 
 const Header = () => {
@@ -13,11 +13,11 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Your API </h1>
             <img className="yapilogo" src={yapilogo} alt="yapi logo"></img>
           </Link>
-          <p className="m-0">Finish your project with your favorite public API's</p>
-          
+          <p className="m-0">
+            Finish your project with your favorite public API's
+          </p>
         </div>
         <div>
           {Auth.loggedIn() ? (
