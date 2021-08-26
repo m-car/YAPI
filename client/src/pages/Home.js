011707ccaba9a25a27317273f8d180627d3a17be
Vivi Cowan <vivicare@gmail.com>
2021-08-26 14:47:17 -0500

    added a search bar to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import "./home.css";
 import { useQuery } from "@apollo/client";
 
-
-
-
 const Home = () => {
-
-return (
-  <h1>hello HOME PAGE</h1>
-)
-
-  
+  return (
+    <section class="hero hero--video">
+      <div class="search">
+        <form>
+          <input
+            class="input"
+            type="text"
+            name="s"
+            placeholder="How may we help you?"
+          />
+          <button type="submit">
+            <FaSearch />
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
