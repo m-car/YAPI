bc1c07c3a229fd8bc2e5d4c38964c37866a9daf4
Marko Caric <pbmc117@gmail.com>
2021-08-26 13:39:40 -0700

    Selected API page renders api info and comments from place holder arrays.

--- client/src/pages/Selected.js
+++ client/src/pages/Selected.js
@@ -1,25 +1,50 @@
 import React, { useState } from "react";
 import ReviewForm from "../components/ReviewForm/index"
-import ReviewList from "../components/ReviewList/index";
+
 
 
 const Selected = () => {
+    const fakeAPI = [
 
+        {
+            title: "Dog API",
+            category: "Animals",
+            description: "An API about dogs and what not.",
+            url: "https://dogsapi.com",
+            auth: "apiKey",
+            https: "Yes",
+            cors: "No",
+        },
+    ]
+
+    const fakeComments = [
+        {
+            username: "user A", rating: 4, comment: "It worked great for my team."
+        },
+        {
+            username: "user B", rating: 1, comment: "where are the docs."
+        },
+        {
+            username: "user C", rating: 3, comment: "i loved it ."
+        },
+    ]
     return (
         <main className="flex-row justify-center  mb-4">
             <div className="col-12 col-lg-10">
                 <h1>SELECTED API PAGE</h1>
                 {/* 1 API INFO */}
-                <div className="card">
-                    <h1 className="card-header bg-dark text-light p-2">yapi api</h1>
-                    <div className="card-body">
-                        <h2>Team Endgame</h2><p>(average star rating)</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
+                {fakeAPI.map((api) => (
+                    <div className="card">
+                        <h1 className="card-header bg-dark text-light p-2">{api.title}</h1>
+                        <div className="card-body">
+                            <h2>Team Endgame</h2><p>Rating : 5 </p>
+                            <p>{api.description}
+                            </p>
+                        </div>
                     </div>
-                </div>
+                ))}
+
+
 
 
                 {/* Review box */}
@@ -31,28 +56,17 @@ const Selected = () => {
                 {/* Reviews list */}
                 <div className="card">
                     <h1 className="card-header bg-dark text-light p-2">Reviews</h1>
-                    {[{
-                        username: "user A", rating: 3, comment: "It worked great for my team."
-                    }]}
-                    {/* User 1 review */}
-                    <div className="card-body">
-                        <h2>User A</h2><p>* * *  </p>
-                        <p>
-                            It worked great for my team.
-                        </p>
-                    </div>
-                    {/* user 2 review  */}
-                    <div className="card-body">
-                        <h2>User B</h2><p>* * * * *</p>
-                        <p>Very easy to use, check out my project at github!
-                        </p>
-                    </div>
-                    {/* user 3 review */}
-                    <div className="card-body">
-                        <h2>User C</h2><p>* * * * *</p>
-                        <p>Will this have long term support?
-                        </p>
-                    </div>
+
+                    {fakeComments.map((comment) => (
+                        <div className="card-body">
+
+                            <h2>{comment.username}</h2><p>{comment.rating}</p>
+                            <p>
+                                {comment.comment}
+                            </p>
+                        </div>
+                    ))}
+
                 </div>
             </div>
         </main>
