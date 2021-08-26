d65b26ba17e5580b930c8c26c7514b64eba2d77a
Marko Caric <pbmc117@gmail.com>
2021-08-26 10:16:39 -0700

    removed review list component from selected api page

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,31 +1,30 @@
+import { makeProcessedFieldsMerger } from "@apollo/client/cache/inmemory/helpers";
 import React from "react";
-
-const CommentList = ({ comments = [] }) => {
+// const reviewSeeds = [
+// {
+//   api: "X",
+//   username: "marko",
+//   rating: 3,
+//   comment: "Wow this app was really helpful"
+// },
+// ]
+const ReviewList = ({ comments = [] }) => {
   if (!comments.length) {
     return <h3>No Reviews Yet</h3>;
   }
 
   return (
     <>
-      <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: "1px dotted #1a1a1a" }}
-      >
+      <h3>
         Comments
       </h3>
-      <div className="flex-row my-4">
+      <div >
         {comments &&
           comments.map((comment) => (
-            <div key={comment._id} className="col-12 mb-3 pb-3">
-              <div className="p-3 bg-dark text-light">
-                <h5 className="card-header">
-                  {comment.commentAuthor} commented{" "}
-                  <span style={{ fontSize: "0.825rem" }}>
-                    on {comment.createdAt}
-                  </span>
-                </h5>
-                <p className="card-body">{comment.commentText}</p>
-              </div>
+            <div className="card-body">
+              <h2>User B</h2><p>* * * * *</p>
+              <p>Very easy to use, check out my project at github!
+              </p>
             </div>
           ))}
       </div>
@@ -33,4 +32,4 @@ const CommentList = ({ comments = [] }) => {
   );
 };
 
-export default CommentList;
+export default ReviewList;
--- client/src/pages/Selected.js
+++ client/src/pages/Selected.js
@@ -1,85 +1,61 @@
-import React ,{ useState }from "react";
+import React, { useState } from "react";
 import ReviewForm from "../components/ReviewForm/index"
-import { useMutation } from "@apollo/client";
-import { ADD_COMMENT } from "../utils/mutations";
-
+import ReviewList from "../components/ReviewList/index";
 
 
 const Selected = () => {
-    const [formState, setFormState] = useState({
-        username: "",
-        comment: "",
-      
-      });
-      const [addComment, { error, data }] = useMutation(ADD_COMMENT);
-      
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-    
-        setFormState({
-          ...formState,
-          [name]: value,
-        });
-    };
-        const handleFormSubmit = async (event) => {
-            event.preventDefault();
-            console.log(formState);
-        
-            try {
-            
-            } catch (e) {
-              console.error(e);
-            }
-          };
 
     return (
-       <main className ="flex-row justify-center  mb-4">
-           <div className="col-12 col-lg-10">
-               <h1>SELECTED API PAGE</h1>
-               {/* 1 API INFO */}
-               <div className="card">
-                   <h1 className="card-header bg-dark text-light p-2">yapi api</h1>
-                   <div className="card-body">
-                       <h2>Team Endgame</h2><p>(average star rating)</p>
-                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                       </p>
-                   </div>
-               </div>
-                
-
-               {/* Review box */}
-              <ReviewForm />
+        <main className="flex-row justify-center  mb-4">
+            <div className="col-12 col-lg-10">
+                <h1>SELECTED API PAGE</h1>
+                {/* 1 API INFO */}
+                <div className="card">
+                    <h1 className="card-header bg-dark text-light p-2">yapi api</h1>
+                    <div className="card-body">
+                        <h2>Team Endgame</h2><p>(average star rating)</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </p>
+                    </div>
+                </div>
 
 
-               
+                {/* Review box */}
+                <ReviewForm />
 
-               {/* Reviews list */}
-               <div className="card">
-                   <h1 className="card-header bg-dark text-light p-2">Reviews</h1>
-                   {/* User 1 review */}
-                   <div className="card-body">
-                       <h2>User A</h2><p>* * *  </p>
-                       <p>
-                            It worked great for my team. 
-                       </p>
-                   </div>
-                   {/* user 2 review  */}
-                   <div className="card-body">
-                       <h2>User B</h2><p>* * * * *</p>
-                       <p>Very easy to use, check out my project at github!
-                       </p>
-                   </div>
-                   {/* user 3 review */}
-                   <div className="card-body">
-                       <h2>User C</h2><p>* * * * *</p>
-                       <p>Will this have long term support?
-                       </p>
-                   </div>
-               </div>
-           </div>
-       </main>
+
+
+
+                {/* Reviews list */}
+                <div className="card">
+                    <h1 className="card-header bg-dark text-light p-2">Reviews</h1>
+                    {[{
+                        username: "user A", rating: 3, comment: "It worked great for my team."
+                    }]}
+                    {/* User 1 review */}
+                    <div className="card-body">
+                        <h2>User A</h2><p>* * *  </p>
+                        <p>
+                            It worked great for my team.
+                        </p>
+                    </div>
+                    {/* user 2 review  */}
+                    <div className="card-body">
+                        <h2>User B</h2><p>* * * * *</p>
+                        <p>Very easy to use, check out my project at github!
+                        </p>
+                    </div>
+                    {/* user 3 review */}
+                    <div className="card-body">
+                        <h2>User C</h2><p>* * * * *</p>
+                        <p>Will this have long term support?
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 
 
