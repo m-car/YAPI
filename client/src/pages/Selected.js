fc39a50013e1e081fb3f4cf842494fbbf3717663
Marko Caric <pbmc117@gmail.com>
2021-08-26 14:11:06 -0700

    Added more tags in the api info html , to display all api info.

--- client/src/pages/Selected.js
+++ client/src/pages/Selected.js
@@ -28,16 +28,25 @@ const Selected = () => {
             username: "user C", rating: 3, comment: "i loved it ."
         },
     ]
+    if (!fakeComments.length) {
+        return <h3>No Reviews Yet</h3>;
+    }
+
     return (
         <main className="flex-row justify-center  mb-4">
             <div className="col-12 col-lg-10">
                 <h1>SELECTED API PAGE</h1>
                 {/* 1 API INFO */}
+
                 {fakeAPI.map((api) => (
                     <div className="card">
                         <h1 className="card-header bg-dark text-light p-2">{api.title}</h1>
                         <div className="card-body">
-                            <h2>Team Endgame</h2><p>Rating : 5 </p>
+                            <h2>Category: {api.category}</h2><p>Rating : 5 </p>
+                            <a href={api.url}>Link to Site</a>
+                            <p>Https: {api.https}</p>
+                            <p>Auth Type: {api.auth}</p>
+                            <p>Cors: {api.cors}</p>
                             <p>{api.description}
                             </p>
                         </div>
@@ -47,23 +56,22 @@ const Selected = () => {
 
 
 
-                {/* Review box */}
+                {/* Review box add context if user is not logged in cant leave review*/}
                 <ReviewForm />
 
 
 
 
                 {/* Reviews list */}
+                <br></br>
                 <div className="card">
                     <h1 className="card-header bg-dark text-light p-2">Reviews</h1>
 
                     {fakeComments.map((comment) => (
                         <div className="card-body">
-
-                            <h2>{comment.username}</h2><p>{comment.rating}</p>
-                            <p>
-                                {comment.comment}
-                            </p>
+                            <h2>{comment.username}</h2>
+                            <p>{comment.rating}</p>
+                            <p>{comment.comment}</p>
                         </div>
                     ))}
 
