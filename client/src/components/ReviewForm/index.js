ee25ce8df52c32cf80ec6017ad09a3a2211b7b1c
Vivi Cowan <vivicare@gmail.com>
2021-08-26 19:21:17 -0500

    imported correct mutation

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 
-import { ADD_COMMENT } from "../../utils/mutations";
+import { ADD_REVIEW } from "../../utils/mutations";
 
 import Auth from "../../utils/auth";
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,64 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import hero from "../assets/images/hero.jpg";
+import yapilogo from "../assets/images/yapi-logo.png";
 import "./home.css";
 import { useQuery } from "@apollo/client";
 
 const Home = () => {
   return (
+    <section class="hero">
+      <header id="header">
+        <div>
+          <Link className="text-light" to="/">
+            <img className="yapilogo" src={yapilogo} alt="yapi logo"></img>
+          </Link>
+        </div>
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/select">
+                {Auth.getProfile().data.username}'s SELECTED API
+              </Link>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </header>
+      <header class="hero-header">
+        <h1 class="hero-title">The title of this hero module</h1>
+      </header>
+      <footer class="hero-footer">
+        <div className="search">
+          <form>
+            <input
+              className="input"
+              type="text"
+              name="s"
+              placeholder="How may we help you?"
+            />
+            <button type="submit">
+              <FaSearch />
+            </button>
+          </form>
+        </div>
+      </footer>
+    </section>
     // <section className="hero hero--video">
-    //   <div className="search">
-    //     <form>
-    //       <input
-    //         className="input"
-    //         type="text"
-    //         name="s"
-    //         placeholder="How may we help you?"
-    //       />
-    //       <button type="submit">
-    //         <FaSearch />
-    //       </button>
-    //     </form>
-    //   </div>
+
     //   <img className="image" src={hero} alt="hero image"></img>
     // </section>
-    <h1>Hehe</h1>
   );
 };
 
